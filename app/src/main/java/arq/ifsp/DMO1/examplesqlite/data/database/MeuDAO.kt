42d05d49de81c844a985cd3f7378a16f8752a92b
Guilherme <guilhermecresppo20@gmail.com>
2025-01-20 09:43:32 -0300

    adicionando a logica de update na aplicação

--- app/src/main/java/arq/ifsp/DMO1/examplesqlite/data/database/MeuDAO.kt
+++ app/src/main/java/arq/ifsp/DMO1/examplesqlite/data/database/MeuDAO.kt
@@ -1,5 +1,6 @@
 package arq.ifsp.DMO1.examplesqlite.data.database
 
+import android.content.ContentValues
 import androidx.core.content.contentValuesOf
 import arq.ifsp.DMO1.examplesqlite.data.model.MeuDado
 
@@ -44,5 +45,24 @@ class MeuDAO(private val dbhelper: DatabaseHelper) {
         }
 
         return dados
+    }
+
+
+    fun  update(meuDado: MeuDado){
+        val values = ContentValues().apply {
+            put(DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO,meuDado.texto)
+        }
+
+        val where = "${DatabaseHelper.DATABASE_KEYS.COLUMN_ID } = ?"
+        val whereArgs = arrayOf(meuDado.id.toString())
+
+        // where args precisa ser compativel com a quantidade de colunas da tabela, no caso o
+        // prepareStatement tem apenas 1 argumento
+
+        val db = dbhelper.writableDatabase
+        db.update(DatabaseHelper.DATABASE_KEYS.COLUMN_TEXTO,
+            values,where,whereArgs)
+
+        // caso nao precise de where args so colocar como null nos argumentos do dbHelper
     }
 }
